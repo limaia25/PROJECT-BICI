8570a7980c5c7336e97a043b97a0d4f92622c8f2
Li Maia <limaia.25@gmail.com>
2018-03-31 00:42:34 +0100

    first commit

---
+++ src/main/java/com/pers/limaia/bici/entities/Pessoa.java
@@ -0,0 +1,37 @@
+package com.pers.limaia.bici.entities;
+
+import java.io.Serializable;
+
+import com.pers.limaia.bici.enums.GeneroEnum;
+
+public class Pessoa implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String nome;
+	private int idade;
+	private GeneroEnum sexo;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public GeneroEnum getSexo() {
+		return sexo;
+	}
+	public void setSexo(GeneroEnum sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/pers/limaia/bici/page/HomePage.java
@@ -0,0 +1,54 @@
+package com.pers.limaia.bici.page;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
+
+import com.pers.limaia.bici.entities.Pessoa;
+
+public class HomePage extends WebPage {
+	private Pessoa pessoaRegisto;
+	private String valorApresentar;
+
+	public HomePage() {
+		setPessoaRegisto(new Pessoa());
+		Form<Void> form = new Form<Void>("form") {
+			private static final long serialVersionUID = 1L;
+
+			protected void onSubmit() {
+				setValorApresentar(getPessoaRegisto().getNome());
+			}
+		};
+
+		Button gravar = new Button("gravar");
+		form.add(gravar);
+		
+		TextField<String> nomeCampo = new TextField<>("nomeCampo", new PropertyModel<String>(pessoaRegisto, "nome"));
+		form.add(nomeCampo);
+
+		TextField<String> mostrarCampo = new TextField<>("mostrarCampo",
+				new PropertyModel<String>(this, "valorApresentar"));
+		mostrarCampo.setEnabled(false);
+		form.add(mostrarCampo);
+
+		add(form);
+	}
+
+	public Pessoa getPessoaRegisto() {
+		return pessoaRegisto;
+	}
+
+	public void setPessoaRegisto(Pessoa pessoaRegisto) {
+		this.pessoaRegisto = pessoaRegisto;
+	}
+
+	public String getValorApresentar() {
+		return valorApresentar;
+	}
+
+	public void setValorApresentar(String valorApresentar) {
+		this.valorApresentar = valorApresentar;
+	}
+}
